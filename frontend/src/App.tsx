92277ac044a8817dfe2a584c9d7631436a6bdc2d
Eliezer Soares <eliezerdrum@gmail.com>
2020-07-23 17:13:35 -0300

    Web Application

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+import Home from './components/pages/Home/index';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
---
+++ frontend/src/components/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const Home = () => {
+  return(
+    <div id="container">
+      <header className="header">
+        Hello World!
+      </header>
+    </div>
+  );
+}
+
+export default Home;
