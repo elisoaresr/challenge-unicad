19888f66c3d4774793823ab3713dc5b118a2c5d8
Eliezer Soares <eliezerdrum@gmail.com>
2020-07-23 15:13:34 -0300

    Web Application

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,8 +5,18 @@ const routes = express.Router()
 
 routes.get('/points', async (request, response) => {
   const items = await knex('items').select('*');
-  
+
   return response.json(items);
+});
+
+routes.post('/points', async (request, response) => {
+  const {
+    name,
+    date,
+    send,
+    done
+  } = request.body;
+
 });
 
 export default routes;
