908c3bbbd8027be38816c58c37e210be1cc906b4
Eliezer Soares <eliezerdrum@gmail.com>
2020-07-27 18:14:12 -0300

    Web Application

--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+
+import api from '../../services/api';
 import Main from '../../template/Main';
 
 const headerProps = {
@@ -9,15 +10,50 @@ const headerProps = {
 }
 
 const Home = (props: any) => {
+  const [item, setItems] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    date: '',
+    send: '',
+    done: ''
+  });
+
+  useEffect(() => {
+    api.get('points').then(response => {
+      console.log(response);
+    });
+  }, []);
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value })
+  }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { name, date, send, done } = formData
+
+    const data = {
+      name,
+      date,
+      send,
+      done
+    };
+    console.log(data);
+  }
+
   return (
     <Main {...headerProps}>
-      <div className="form">
+      <div className="form" onSubmit={handleSubmit}>
         <div className="row">
           <div className="col-12 col-md-6">
             <div className="form group">
               <label>Nome</label>
               <input type="text" className="form-control"
-                name="name" placeholder="Digite o nome" />
+                name="name" placeholder="Digite o nome"
+                onChange={handleInputChange} />
             </div>
           </div>
 
@@ -25,25 +61,26 @@ const Home = (props: any) => {
             <div className="form group">
               <label>Data de Entrega</label>
               <input type="date" className="form-control"
-                name="date" placeholder="Digite a data de Entrega" />
+                name="date" placeholder="Digite a data de Entrega"
+                onChange={handleInputChange} />
             </div>
           </div>
-
 
           <div className="col-12 col-md-6">
             <div className="form group">
               <label>Ponto de Partida</label>
               <input type="text" className="form-control"
-                name="send" placeholder="Digite o endereço" />
+                name="send" placeholder="Digite o endereço"
+                onChange={handleInputChange} />
             </div>
           </div>
-
 
           <div className="col-12 col-md-6">
             <div className="form group">
               <label>Ponto de destino</label>
               <input type="text" className="form-control"
-                name="done" placeholder="Digite o endereço" />
+                name="done" placeholder="Digite o endereço"
+                onChange={handleInputChange} />
             </div>
           </div>
         </div>
